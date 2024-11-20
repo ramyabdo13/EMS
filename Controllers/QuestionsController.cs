c506697a98d73106ca7f1efe87d66be8c3dd8549
Administrator <Administrator@DESKTOP-MMBMKTC>
2024-11-20 21:08:55 +0300

    Add project files.

---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EMS.Models;
+ 
+
+namespace EMS.Controllers
+{
+    public class QuestionsController : Controller
+    {
+          EMSDBContext _context= new EMSDBContext();
+
+        //public QuestionsController(EMSDBContext context)
+        //{
+        //    _context = context;
+        //}
+
+        // GET: Questions
+        public  async Task<IActionResult> Index()
+        {
+           
+
+            List< Question> qus =   await  _context.Questions.ToListAsync ();
+            return View(qus);
+        }
+         
+        // GET: Questions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Questions
+                .Include(q => q.Exam)
+                .FirstOrDefaultAsync(m => m.QuestionId == id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return View(question);
+        }
+
+        // GET: Questions/Create
+        public IActionResult Create()
+        {
+            ViewData["ExamId"] = new SelectList(_context.Exams, "ExamId", "ExamId");
+            //return View();
+
+            return PartialView("_CreateQuestionPartial");
+        }
+
+        // POST: Questions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("QuestionId,ExamId,QuestionContent,QuestionType,QuestionMark,NumberOfAnswers")] Question question)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(question);
+                await _context.SaveChangesAsync();
+                //return RedirectToAction(nameof(Index));
+                return Json(new { success = true });
+            }
+            ViewData["ExamId"] = new SelectList(_context.Exams, "ExamId", "ExamId", question.ExamId);
+            return View(question);
+        }
+
+        // GET: Questions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+            ViewData["ExamId"] = new SelectList(_context.Exams, "ExamId", "ExamId", question.ExamId);
+            return View(question);
+        }
+
+        // POST: Questions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("QuestionId,ExamId,QuestionContent,QuestionType,QuestionMark,NumberOfAnswers")] Question question)
+        {
+            if (id != question.QuestionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(question);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuestionExists(question.QuestionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ExamId"] = new SelectList(_context.Exams, "ExamId", "ExamId", question.ExamId);
+            return View(question);
+        }
+
+        // GET: Questions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var question = await _context.Questions
+                .Include(q => q.Exam)
+                .FirstOrDefaultAsync(m => m.QuestionId == id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return View(question);
+        }
+
+        // POST: Questions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var question = await _context.Questions.FindAsync(id);
+            if (question != null)
+            {
+                _context.Questions.Remove(question);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuestionExists(int id)
+        {
+            return _context.Questions.Any(e => e.QuestionId == id);
+        }
+    }
+}
---
+++ Migrations/20240903064301_migrationEMSDB.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrationEMSDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Exams",
+                columns: table => new
+                {
+                    ExamID = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ExamName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ExamTime = table.Column<byte>(type: "tinyint", nullable: true),
+                    ExamMarke = table.Column<byte>(type: "tinyint", nullable: true),
+                    Min_Marke = table.Column<byte>(type: "tinyint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exams", x => x.ExamID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    QuestionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ExamID = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    QuestionContent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QuestionType = table.Column<string>(type: "nchar(10)", fixedLength: true, maxLength: 10, nullable: true),
+                    QuestionMark = table.Column<byte>(type: "tinyint", nullable: true),
+                    NumberOfAnswers = table.Column<byte>(type: "tinyint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.QuestionID);
+                    table.ForeignKey(
+                        name: "FK_Questions_Exams_ExamID",
+                        column: x => x.ExamID,
+                        principalTable: "Exams",
+                        principalColumn: "ExamID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Answers",
+                columns: table => new
+                {
+                    AnswerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Answercontent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdelAnswer = table.Column<bool>(type: "bit", nullable: false),
+                    QuestionID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answers", x => x.AnswerID);
+                    table.ForeignKey(
+                        name: "FK_Answers_Questions_QuestionID",
+                        column: x => x.QuestionID,
+                        principalTable: "Questions",
+                        principalColumn: "QuestionID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_QuestionID",
+                table: "Answers",
+                column: "QuestionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_ExamID",
+                table: "Questions",
+                column: "ExamID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answers");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Exams");
+        }
+    }
+}
